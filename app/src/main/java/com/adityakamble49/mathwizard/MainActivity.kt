d931741432a939cbdb314467d9b3806714513bdd
Aditya Kamble <adityakamble49@gmail.com>
2019-12-20 21:46:06 -0500

    Math Symbol Custom font added | Main Screen update

--- app/src/main/java/com/adityakamble49/mathwizard/MainActivity.kt
+++ app/src/main/java/com/adityakamble49/mathwizard/MainActivity.kt
@@ -3,10 +3,12 @@ package com.adityakamble49.mathwizard
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import com.adityakamble49.mathwizard.classifier.MathSymbolClassifier
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -17,15 +19,29 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private val mathSymbolClassifier = MathSymbolClassifier(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setup Actionbar
+        setSupportActionBar(main_toolbar)
 
         // Setup Draw View
         draw_view?.setStrokeWidth(70.0f)
         draw_view?.setColor(Color.WHITE)
         draw_view?.setBackgroundColor(Color.BLACK)
 
+        // Setup Clear Canvas FAB
+        fab_reset_canvas.setOnClickListener {
+            draw_view?.clearCanvas()
+            math_symbol_predicted.setText(R.string.draw_math_symbol)
+        }
 
         draw_view?.setOnTouchListener { _, event ->
             draw_view?.onTouchEvent(event)
@@ -34,10 +50,19 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        // Setup Math Symbol Classifier
+        mathSymbolClassifier.initializeInterpreterTask().addOnFailureListener {
+            Log.e(TAG, "Math Symbol Classifier Initialization Failed !")
+        }
     }
 
     private fun classifyDrawing() {
+        val drawnImageBitmap = draw_view?.getBitmap()
 
+        if (drawnImageBitmap != null && mathSymbolClassifier.isInitialized) {
+            mathSymbolClassifier.classify(drawnImageBitmap)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/adityakamble49/mathwizard/classifier/MathSymbolClassifier.kt
+++ app/src/main/java/com/adityakamble49/mathwizard/classifier/MathSymbolClassifier.kt
@@ -1,4 +1,44 @@
 package com.adityakamble49.mathwizard.classifier
 
-class MathSymbolClassifier  {
+import android.content.Context
+import android.graphics.Bitmap
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks.call
+import org.tensorflow.lite.Interpreter
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
+class MathSymbolClassifier(private val context: Context) {
+
+    companion object {
+        private const val TAG = "MathSymbolClassifier"
+        private const val MODEL_FILE = "math_symbols_model.tflite"
+        private const val FLOAT_TYPE_SIZE = 4
+        private const val PIXEL_SIZE = 1
+        private const val OUTPUT_CLASSES_COUNT = 82
+    }
+
+    private var intrepreter: Interpreter? = null
+    val isInitialized = false
+
+    private val executorService = Executors.newCachedThreadPool()
+
+    private var imageWidth = 0
+    private var imageHeight = 0
+    private var modelInputSize = 0
+
+    fun initializeInterpreterTask(): Task<Void> {
+        return call(executorService, Callable<Void> {
+            initializeInterpreter()
+            null
+        })
+    }
+
+    private fun initializeInterpreter() {
+
+    }
+
+    fun classify(imageBitmap: Bitmap) {
+
+    }
 }
